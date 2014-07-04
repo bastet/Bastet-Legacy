38e7fc02f32b0756aca8442ab81efdc0ba93c65c
Martin Evans <martindevans@gmail.com>
2014-07-04 01:26:52 +0100

    Added a proxying module which takes /devices/{id}/{SOME PATH} and then passes the request on to the appropriate node, then sends the response back as a HTTP response (full proxy for coap)
    Signed-off-by: Martin Evans <martindevans@gmail.com>

---
+++ Bastet/HttpServer/Modules/DeviceProxying.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Data;
+using Bastet.Database.Model;
+using Nancy;
+using ServiceStack.OrmLite;
+
+namespace Bastet.HttpServer.Modules
+{
+    public class DevicesProxyModule
+        : NancyModule
+    {
+        public const string PATH = "/devices/{id}";
+
+        private readonly IDbConnection _connection;
+        //private HttpTranslator
+
+        public DevicesProxyModule(IDbConnection connection)
+            : base(PATH)
+        {
+            _connection = connection;
+
+            Get["/{endpoint*}"] = ProxyGet;
+            Put["/{endpoint*}"] = ProxyPut;
+            Post["/{endpoint*}"] = ProxyPost;
+            Delete["/{endpoint*}"] = ProxyDelete;
+        }
+
+        private dynamic ProxyGet(dynamic parameters)
+        {
+            var device = _connection.SingleById<Device>((int)parameters.id);
+            if (device == null)
+                return Negotiate
+                    .WithStatusCode(HttpStatusCode.NotFound)
+                    .WithModel(new { Error = string.Format("Unknown Device ID ({0})", parameters.id) });
+
+            throw new NotImplementedException();
+        }
+
+        private dynamic ProxyPut(dynamic parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        private dynamic ProxyPost(dynamic parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        private dynamic ProxyDelete(dynamic parameters)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
