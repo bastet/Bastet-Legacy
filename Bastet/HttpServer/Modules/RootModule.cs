33e4698c3f9805e702695bb3636b2c53a85565c9
Martin Evans <martindevans@gmail.com>
2015-02-09 01:03:41 +0000

    Added the beginnings of an initial setup system (in browser). Need some way to store setup progress in DB, and then continue setup if page is reloaded
    Signed-off-by: Martin Evans <martindevans@gmail.com>

--- Bastet/HttpServer/Modules/RootModule.cs
+++ Bastet/HttpServer/Modules/RootModule.cs
@@ -1,13 +1,37 @@
-﻿using Nancy;
+﻿using System.Data;
+using Nancy;
 
 namespace Bastet.HttpServer.Modules
 {
     public class RootModule
         : NancyModule
     {
-        public RootModule()
+        private readonly IDbConnection _connection;
+
+        public RootModule(IDbConnection connection)
         {
-            Get["/"] = _ => View["Root"];
+            _connection = connection;
+
+            Get["/"] = GetRoot;
+        }
+
+        private dynamic GetRoot(dynamic parameters)
+        {
+            var setup = GetSetupProgress();
+            if (setup.HasValue)
+                return View["Setup", setup];
+            else
+                return View["Root"];
+        }
+
+        private SetupProgress? GetSetupProgress()
+        {
+            return null;
+        }
+
+        public struct SetupProgress
+        {
+
         }
     }
 }
