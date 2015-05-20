0dfe601087a4d6b5033d2766341a9268fac29051
Martin Evans <martindevans@gmail.com>
2015-05-20 04:32:03 +0100

    Added basis of http backend (which is abstract) and hive backend (which is a concrete http backend)
    Signed-off-by: Martin Evans <martindevans@gmail.com>

---
+++ Bastet/Backends/Http/BaseHttpBackend.cs
@@ -0,0 +1,40 @@
+﻿using Bastet.Database.Model;
+using System;
+using System.Threading.Tasks;
+
+namespace Bastet.Backends.Http
+{
+    public abstract class BaseHttpBackend
+        : IBackend
+    {
+        private readonly Uri _baseUri;
+        private readonly bool _useCoreLink;
+        private readonly string _swaggerPath;
+
+        public BaseHttpBackend(Uri baseUri, bool useCoreLink = false, string swaggerPath = "swagger.json")
+        {
+            _baseUri = baseUri;
+            _useCoreLink = useCoreLink;
+            _swaggerPath = swaggerPath;
+        }
+
+        public async Task<IBackendResponse> Request(Device device, BackendRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public abstract Task<BackendDescription> Describe(Device device);
+
+        protected static Task<BackendDescription> FetchWellKnownCore(Uri baseUri)
+        {
+            throw new NotImplementedException("Fetch .well-known/core");
+            throw new NotImplementedException("Turn this into a BackendDescription");
+        }
+
+        protected static Task<BackendDescription> FetchSwagger(Uri baseUri, string swaggerPath)
+        {
+            throw new NotImplementedException("Fetch swaggerPath");
+            throw new NotImplementedException("Turn this into a BackendDescription");
+        }
+    }
+}
---
+++ Bastet/Backends/Http/Hive/HiveBackend.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bastet.Backends.Http.Hive
+{
+    public class HiveBackend
+        : BaseHttpBackend
+    {
+    }
+}
